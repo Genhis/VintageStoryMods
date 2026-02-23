46d1d02bab00bb0471d430510b1f47702b77e1c6
Genhis <genhis1@gmail.com>
2026-02-23 17:41:48 +0100

    Add basic support for Geology Map mod

--- Mapper/Patches/Mods/GiMap.cs
+++ Mapper/Patches/Mods/GiMap.cs
@@ -3,12 +3,10 @@ namespace Mapper.Patches.Mods.GiMap;
 using HarmonyLib;
 using Mapper.Util.Harmony;
 using Mapper.Util.Reflection;
-using Mapper.WorldMap;
 using System;
 using System.Collections.Generic;
 using System.Reflection;
 using System.Reflection.Emit;
-using Vintagestory.API.Client;
 using Vintagestory.API.Common;
 using Vintagestory.API.Datastructures;
 using Vintagestory.API.MathTools;
@@ -23,38 +21,22 @@ internal static class AMapLayer {
 	[DynamicHarmonyPatch("OnLoaded")]
 	[HarmonyPostfix]
 	internal static void OnLoaded(MapLayer __instance, ICoreAPI ___api, UniqueQueue<FastVec2i> ____chunksToGen, object ____chunksToGenLock) {
-		MapperChunkMapLayer mapperLayer = MapperChunkMapLayer.GetInstance(___api);
-		lock(mapperLayer.OnChunkChanged)
-			mapperLayer.OnChunkChanged[__instance] = chunkPosition => {
-				lock(____chunksToGenLock)
-					____chunksToGen.Enqueue(chunkPosition);
-			};
+		ModPatchUtil.OnLoaded(__instance, ___api, ____chunksToGen, ____chunksToGenLock);
 	}
 
 	[DynamicHarmonyPatch("OnShutDown")]
 	[HarmonyPostfix]
 	internal static void OnShutDown(MapLayer __instance, ICoreAPI ___api) {
-		MapperChunkMapLayer? mapperLayer = MapperChunkMapLayer.GetInstance(___api);
-		if(mapperLayer != null)
-			lock(mapperLayer.OnChunkChanged)
-				mapperLayer.OnChunkChanged.Remove(__instance);
+		ModPatchUtil.OnShutDown(__instance, ___api);
 	}
 
 	// This patch must go before OnOffThreadTick, otherwise it is ignored in some cases (on the first game launch).
 	[DynamicHarmonyPatch("LoadFromChunkPixels")]
 	[HarmonyPrefix]
 	internal static bool LoadFromChunkPixels(MapLayer __instance, FastVec2i cord, ref int[] pixels, ICoreAPI ___api) {
-		int? scaleFactor = MapperChunkMapLayer.GetInstance(___api).GetScaleFactor((IClientPlayer?)null, cord);
-		if(scaleFactor == null)
-			return false;
-		if(scaleFactor != 1 && __instance.LayerGroupCode != "chunk-grid")
-			pixels = MapperChunkMapLayer.ApplyBoxFilter((int[])pixels.Clone(), (uint)scaleFactor);
-		return true;
+		return ModPatchUtil.LoadFromChunkPixels(cord, ref pixels, ___api, __instance.LayerGroupCode != "chunk-grid");
 	}
 
-	// OnOffThreadTick patch is not strictly necessary since we are patching LoadFromChunkPixels,
-	// but it prevents processing chunks unnecessarily when they wouldn't be shown anyway,
-	// which is especially helpful for the very slow OreMapLayer.
 	[DynamicHarmonyPatch("OnOffThreadTick", Optional = true)]
 	[HarmonyTranspiler]
 	internal static IEnumerable<CodeInstruction> OnOffThreadTick(IEnumerable<CodeInstruction> instructions, ILGenerator generator) {
@@ -78,33 +60,6 @@ internal static class AMapLayer {
 				new(OpCodes.Call, typeof(Math).GetCheckedMethod("Min", BindingFlags.Static, [typeof(int), typeof(int)])),
 			]);
 
-		// Find https://github.com/GinoxXP/gi-map/blob/b4e647d54b2532f4daa065dac7c64504a3e619a7/GiMap/Client/AMapLayer.cs#L220
-		// Remember `cord` store instruction
-		CodeInstruction cordStloc = matcher.MatchEndForward([
-			new(OpCodes.Ldarg_0),
-			new(OpCodes.Ldfld, chunksToGenField),
-			new(OpCodes.Callvirt, typeof(UniqueQueue<FastVec2i>).GetCheckedMethod("Dequeue", BindingFlags.Instance, [])),
-			CodeMatch.IsStloc(),
-		]).ThrowIfInvalid("Could not find `AMapLayer.OnOffThreadTick()::cord`").Instruction;
-
-		// Find https://github.com/GinoxXP/gi-map/blob/b4e647d54b2532f4daa065dac7c64504a3e619a7/GiMap/Client/AMapLayer.cs#L223
-		// Append `|| !Mapper.Patches.Mods.GiMap.AMapLayer.HasChunk(this.api, cord)` to the condition
-		CodeInstruction skipChunk = matcher.MatchEndForward([
-			new(OpCodes.Callvirt, typeof(IBlockAccessor).GetCheckedMethod("IsValidPos", BindingFlags.Instance, [typeof(int), typeof(int), typeof(int)])),
-			new(OpCodes.Brfalse),
-		]).ThrowIfInvalid("Could not find `AMapLayer.OnOffThreadTick()::IsValidPos()` to patch").Instruction;
-		matcher.Advance(1).InsertAndAdvance([
-			new(OpCodes.Ldarg_0),
-			new(OpCodes.Ldfld, typeof(MapLayer).GetCheckedField("api", BindingFlags.Instance)),
-			CodeInstruction.LoadLocal(cordStloc.LocalIndex()),
-			new(OpCodes.Call, typeof(AMapLayer).GetCheckedMethod("HasChunk", BindingFlags.Static, [typeof(ICoreAPI), typeof(FastVec2i)])),
-			skipChunk.Clone(),
-		]);
-
-		return matcher.InstructionEnumeration();
-	}
-
-	internal static bool HasChunk(ICoreAPI api, FastVec2i chunkPosition) {
-		return MapperChunkMapLayer.GetInstance(api).GetScaleFactor((IClientPlayer?)null, chunkPosition) != null;
+		return ModPatchUtil.OnOffThreadTickTranspiler(matcher, "AMapLayer", chunksToGenField).InstructionEnumeration();
 	}
 }
--- Mapper/readme.md
+++ Mapper/readme.md
@@ -28,4 +28,5 @@ This mod adds map drawing mechanic (cartography) - the world map no longer updat
 
 ## Built-in mod compatibility
 - [Auto Map Markers](https://mods.vintagestory.at/automapmarkers)
+- [Geology Map](https://mods.vintagestory.at/geologymap) - You don't need to repaint the map to update layers from this mod and its layers are not stored in the cartography table.
 - [GiMap](https://mods.vintagestory.at/gimap) - You don't need to repaint the map to update layers from this mod and its layers are not stored in the cartography table.
