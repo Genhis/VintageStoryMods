b4b963fd891b4242305bd2b68c5a027181fbfbab
Genhis <genhis1@gmail.com>
2026-04-04 11:05:10 +0200

    Fix clear debug command not refreshing visible chunks

--- Mapper/WorldMap/MapperChunkMapLayerDebug.cs
+++ Mapper/WorldMap/MapperChunkMapLayerDebug.cs
@@ -1,7 +1,12 @@
 #if DEBUG
 namespace Mapper.WorldMap;
 
+using System.Collections.Concurrent;
+using System.Collections.Generic;
 using Vintagestory.API.Common;
+using Vintagestory.API.Datastructures;
+using Vintagestory.API.MathTools;
+using Vintagestory.GameContent;
 
 public partial class MapperChunkMapLayer {
 	private void RegisterDebugCommands() {
@@ -17,15 +22,36 @@ public partial class MapperChunkMapLayer {
 			this.mapper = mapper;
 		}
 
+		private void RedrawMap() {
+			ConcurrentDictionary<FastVec2i, MultiChunkMapComponent> loadedMapData = MapperChunkMapLayer.loadedMapData.GetValue(this.mapper);
+			foreach(MultiChunkMapComponent value in loadedMapData.Values)
+				value.ActuallyDispose();
+			loadedMapData.Clear();
+
+			UniqueQueue<FastVec2i> chunksToGen = MapperChunkMapLayer.chunksToGen.GetValue(this.mapper);
+			object chunksToGenLock = MapperChunkMapLayer.chunksToGenLock.GetValue(this.mapper);
+			HashSet<FastVec2i> curVisibleChunks = MapperChunkMapLayer.curVisibleChunks.GetValue(this.mapper);
+			lock(chunksToGenLock)
+				foreach(FastVec2i curVisibleChunk in curVisibleChunks)
+					chunksToGen.Enqueue(curVisibleChunk);
+		}
+
 		public TextCommandResult Clear(TextCommandCallingArgs args) {
+			if(!this.mapper.Enabled)
+				return TextCommandResult.Error("Error: Mod disabled");
+
 			if(this.mapper.serverStorage != null) {
 				this.mapper.serverStorage[args.Caller.Player.PlayerUID].Regions.Clear();
+				this.mapper.dirty = true;
 			}
-			else
+			else {
 				lock(this.mapper.clientStorage!.SaveLock) {
 					this.mapper.clientStorage.Chunks.Clear();
 					this.mapper.clientStorage.ChunksToRedraw.Clear();
+					this.mapper.dirty = true;
 				}
+				this.RedrawMap();
+			}
 			return TextCommandResult.Success("Done!");
 		}
 	}
